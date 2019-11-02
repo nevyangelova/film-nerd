53779e23ec3b0aee4249840599787a2b0ca67536
Nevyana <nevyana@Nevyanas-MBP.lan>
2019-11-02 17:47:59 +0000

    Add fake data source and define graphql types

--- backend/src/resolverMap.ts
+++ backend/src/resolverMap.ts
@@ -1,9 +1,21 @@
-import { IResolvers } from "graphql-tools";
-const resolverMap: IResolvers = {
-  Query: {
-    helloWorld(_: void, args: void): string {
-      return `👋 Hello world! 👋`;
-    }
-  }
+import { IResolvers } from 'graphql-tools';
+import faker from 'faker/locale/en';
+import { Movie_Ext } from './movies/types';
+
+const fakeMovie = (): Movie_Ext => ({
+    Title: faker.name.findName(),
+    Year: faker.date.past().toString(),
+});
+
+const resolvers: IResolvers = {
+    Query: {
+        helloWorld(_: void, args: void): string {
+            return `👋 Hello world! 👋`;
+        },
+        fakeMovie(_: void, args: void): Movie_Ext {
+            return fakeMovie();
+        },
+    },
 };
-export default resolverMap;
+
+export default resolvers;
